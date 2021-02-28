72b972fec9a85a7fe07730dc072e6f108a948c3d
Alija Sabic <sabic.alija@gmail.com>
2021-02-28 23:46:41 +0100

    Publish smart living lab website

---
+++ assets/js/8.e70c7327.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{369:function(t,e,n){"use strict";n.r(e);var s={name:"VRTour"},u=n(41),a=Object(u.a)(s,(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._t("default")],2)}),[],!1,null,"56c937d0",null);e.default=a.exports}}]);
